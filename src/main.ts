580690eacc0fa1c7ee335f887763109a5f72ace5
Senner007 <nielshtg@htomail.com>
2023-11-03 15:17:36 +0100

    sql get

--- src/main.ts
+++ src/main.ts
@@ -6,84 +6,39 @@
 import { parse, HTMLElement } from "node-html-parser";
 import { createObjectCsvWriter } from "csv-writer";
 import { has_key_in_latest, read_new_links, store_latest_to_json } from "./store";
-import { sqlite_open, sqlite_insert, sqlite_create_table } from "./sql";
+import { sqlite_open, sqlite_insert, sqlite_create_table, sqlite_get } from "./sql";
+import { parseFuncs, parseFuncsKeys } from "./parsers";
 
 
-;(async () => {
+; (async () => {
   try {
     const db = await sqlite_open();
     // const info = await db.get(`PRAGMA table_info(links)`)
     // console.log(info)
     await sqlite_create_table(db)
-    await sqlite_insert(db, ["fofdo"], "tv2.dk");
+    // await sqlite_insert(db, ["fofdo", "fsdfds"], "tv2.dk");
+    const val = await sqlite_get(db, "fdofdo")
+    console.log(val)
   } catch (err) {
     console.log(err)
   }
 })();
 
 
-//@ts-ignore
-function parseDR(parsed: HTMLElement) : [string, string, string] {
-
-  const label =  parsed.querySelector(".dre-teaser-meta-label.dre-teaser-meta-label--primary")?.text!
-  ||  parsed.querySelector(".dre-article-title-section-label__title--link")?.text!
-
-  const header =
-    parsed.querySelector(".dre-title-text")?.text!
-
-  const subHeader =
-    parsed.querySelector(".dre-article-title__summary")?.text!
-    || 
-    parsed.querySelector(".hydra-latest-news-page-short-news-article__paragraph.dre-variables")?.text!
-    
-
-    return [
-      label, header, subHeader
-    ]
-
-}
-
-//@ts-ignore
-function parseTV2(parsed: HTMLElement) : [string, string, string] {
-  
-  const label = parsed.querySelector(".tc_page__label")?.childNodes[0].text!;
-
-  const header: string = parsed.querySelector(".tc_heading.tc_heading--2")?.childNodes[0].text!;
-  
-  const subHeader =
-    parsed.querySelector(".tc_page__body__standfirst")?.childNodes[0].childNodes[0].text! ||
-    parsed.querySelector(".tc_richcontent")?.firstChild.text!;
-
-  if (subHeader.toLowerCase().includes("podcast") || header.toLowerCase().includes("podcast")) {
-    throw new Error("Includes podcast");
-  }
-
-  return [
-    label, header, subHeader
-  ]
-
-}
 
 
-function veryfy_result(results : [string,string,string]) {
+
+function veryfy_result(results: [string, string, string]) {
   for (const s of results) {
     if (typeof s != "string") {
       throw new Error("invalid data!")
     }
-  } 
+  }
 }
 
-const parseFuncs = {
-  "dr.dk" : parseDR,
-  "tv2.dk" : parseTV2
-} as const
 
+type linkRecord = { category: string, headline: string, subheading: string, link: string }[]
 
-type parseFuncsKeys = keyof typeof parseFuncs
-
-type linkRecord = {category : string, headline : string, subheading: string, link : string}[]
-
-//@ts-ignore
 async function scrape() {
 
   const links = (Array.from(new Set(read_new_links())) as string[]).
@@ -107,11 +62,11 @@ async function scrape() {
       const json = await result.text();
       const parsed: HTMLElement = parse(json);
       const [label, header, subHeader] = parseFunc(parsed)
-    
+
       veryfy_result([label, header, subHeader])
       console.log(label, header, subHeader)
 
-      records.push({ category: label, headline: header, subheading: subHeader, link : aa });
+      records.push({ category: label, headline: header, subheading: subHeader, link: aa });
     } catch (e) {
       console.log(e);
     }
@@ -133,7 +88,7 @@ async function writeCsv(records: linkRecord, filename: string) {
       { id: "category", title: "Category" },
       { id: "headline", title: "Headline" },
       { id: "subheading", title: "SubHeading" },
-      { id : "link", title : "Link" }
+      { id: "link", title: "Link" }
     ],
     encoding: "utf8",
   });
@@ -152,46 +107,3 @@ async function writeCsv(records: linkRecord, filename: string) {
 //     console.log(err)
 //   }
 // })();
-
-
-
-// ;(async () => {
-//   const jsonPreset = (await import('lowdb/node')).JSONPreset
-
-//   type Data = {
-//     messages: string[]
-//   }
-
-//   const defaultData: Data = { messages: [] }
-//   const db = await jsonPreset<Data>('db.json', defaultData)
-    
-//   db.data.messages.push('foo') // ✅ Success
-
-//   // Finally write db.data content to file
-//   await db.write()
-
-// })();
-
-
-// let arr = [
-//     ['sdfsdf sdfsd ', '', "efter danske adasd sdfsdf "],
-//     ['Danmark lider nederlag', "Danmark slår Sverige", "Danmark slog Sverige", "kunne ikke slå", "tabte stort til"],
-
-//     [' sdf sdf til danske dasd', ' sffds OL finaler sdfds', ' sdfdsf fdf finalerne adasd', " på sdfdsfsf finalen" ]
-// ];
-
-// function getCombn(arr: any, pre: any) {
-//     pre = pre || '';
-
-//     if (!arr.length) {
-//         return pre;
-//     }
-
-//     let ans = arr[0].reduce(function (ans: any, value: any) {
-//         return ans.concat(getCombn(
-//             arr.slice(1), pre + value));
-//     }, []);
-//     return ans;
-// }
-//  //@ts-ignore
-// console.log(getCombn(arr));
--- src/sql.ts
+++ src/sql.ts
@@ -25,6 +25,13 @@ class DB_CREATED_ERROR extends Error {
     }
 }
 
+class DB_VALUE_NOT_IN_TABLE extends Error {
+    constructor(msg: string = "Table does not have the value") {
+        super(msg);
+    }
+}
+
+
 export async function sqlite_open() {
 
     try {
@@ -46,32 +53,34 @@ export async function sqlite_open() {
 
 export async function sqlite_create_table(db: Database<sqlite3.Database, sqlite3.Statement>) {
 
-    try {
-        const if_exists = `SELECT * FROM sqlite_master WHERE name ='${LINKS_DATABASE.name}' and type='table'`
-        const table_exists = await db.get(if_exists)
-        if (table_exists) {
-            return
-        }
-        // create table
-        const sql = `CREATE TABLE  ${LINKS_DATABASE.name}(id INTEGER PRIMARY KEY, ${LINKS_DATABASE.link_column} NVARCHAR(500) NOT NULL UNIQUE, date TEXT , host NVARCHAR(100) NOT NULL)`
-        const idx_links = `CREATE INDEX idx_links ON ${LINKS_DATABASE.name}(${LINKS_DATABASE.link_column})`
-        await db.exec(sql)
-        await db.exec(idx_links)
-    } catch (err: unknown) {
-        console.log(err)
-        throw new Error()
+    const if_exists = `SELECT * FROM sqlite_master WHERE name ='${LINKS_DATABASE.name}' and type='table'`
+    const table_exists = await db.get(if_exists)
+    if (table_exists) {
+        return
     }
+    // create table
+    const sql = `CREATE TABLE  ${LINKS_DATABASE.name}(id INTEGER PRIMARY KEY, ${LINKS_DATABASE.link_column} NVARCHAR(500) NOT NULL UNIQUE, date TEXT , host NVARCHAR(100) NOT NULL)`
+    const idx_links = `CREATE INDEX idx_links ON ${LINKS_DATABASE.name}(${LINKS_DATABASE.link_column})`
+    await db.exec(sql)
+    await db.exec(idx_links)
+
 }
 
 export async function sqlite_insert(db: Database<sqlite3.Database, sqlite3.Statement>, links: string[], host: string) {
 
-    try {
-        for (const l of links) {
-            const sql = `INSERT INTO ${LINKS_DATABASE.name}(${LINKS_DATABASE.link_column}, date, host) VALUES ('${l}', '${Date.now()}', '${host}')`
-            await db.exec(sql)
-        }
-    } catch (err) {
-        console.log(err)
-        throw new Error();
+    for (const l of links) {
+        await db.run(
+            `INSERT INTO ${LINKS_DATABASE.name}(${LINKS_DATABASE.link_column}, date, host) VALUES (?, ?, ?)`,
+            l, Date.now(), host
+        )
     }
+}
+
+export async function sqlite_get(db: Database<sqlite3.Database, sqlite3.Statement>, link: string) {
+
+    const result = await db.get(`SELECT ${LINKS_DATABASE.link_column} FROM ${LINKS_DATABASE.name} WHERE ${LINKS_DATABASE.link_column} = ?`, link)
+    if (result === undefined) {
+        throw new DB_VALUE_NOT_IN_TABLE()
+    }
+    return result
 }
